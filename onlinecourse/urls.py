348083f02d64360e05601b2e1621c2cc702a6f00
Rohit-s-Nikam <rohitnikam709@gmail.com>
2026-09-19 02:35:29 +0530

    Update URL patterns for course-related views
    Added URL patterns for course enrollment, submission, and exam results.

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -7,7 +7,34 @@ app_name = 'onlinecourse'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
-    # name the URL
+    # name the URLfrom django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
+
+urlpatterns = [
+    path('', views.CourseListView.as_view(), name='index'),
+    path(
+        '<int:pk>/',
+        views.CourseDetailView.as_view(),
+        name='course_details'
+    ),
+    path(
+        '<int:course_id>/enroll/',
+        views.enroll,
+        name='enroll'
+    ),
+    path(
+        '<int:course_id>/submit/',
+        views.submit,
+        name='submit'
+    ),
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
+]
     path(route='', view=views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
